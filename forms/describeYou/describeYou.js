02d2797ab7a8fd96fdf3fca2fd3e97cfbb6db04d
Makala Smith <mms96354@creighton.edu>
2021-03-29 14:43:57 -0500

    finished describe and exercises

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,9 @@
 
+btnFavExercises.onclick=function(){
+  ChangeForm(favExercises)
+}
 
-
-
-
+rdbtnPersonality.onchange=function(){
+  let userChoice = $("input[name=rdbtnPersonality]:checked").prop("value")
+  console.log(`I would agree that you are a ${userChoice} person too!`)
+}
--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,9 +2,14 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+    for (i = 0; i < exercises.length; i++) 
+        selExercises.addItem(exercises[i])
 }
 
 
 
 
+
+btnMobileNav.onclick=function(){
+  ChangeForm(mobileNav)
+}
