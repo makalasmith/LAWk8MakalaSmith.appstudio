0f31a53bb9fec31191661f90778c2e46269f0054
Makala Smith <mms96354@creighton.edu>
2021-03-29 14:55:59 -0500

    finished the exercises

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -6,10 +6,17 @@ favExercises.onshow=function(){
         selExercises.addItem(exercises[i])
 }
 
-
-
-
-
 btnMobileNav.onclick=function(){
   ChangeForm(mobileNav)
+}
+
+btnDone.onclick=function(){
+  let message = ""
+  for (i = 0; i < selExercises.text.length; i++)
+     message = message + selExercises.text[i] + ", "
+     
+  if(message == "situps, plank, ")
+    console.log("You chose situps and planks - those are the two core exercises.")
+  else
+    console.log(`You did not pick the two core exercises.`)
 }
